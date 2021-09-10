14ed5a672ca075ad3d120476e1d84a4eafd06d50
publicdevop2019 <publicdevop2019hw@gmail.com>
2021-09-10 18:24:48 -0400

    add domainId and name to events

--- src/main/java/com/mt/mall/domain/model/sku/event/SkuPatchedReplyEvent.java
+++ src/main/java/com/mt/mall/domain/model/sku/event/SkuPatchedReplyEvent.java
@@ -1,5 +1,6 @@
 package com.mt.mall.domain.model.sku.event;
 
+import com.mt.common.domain.model.domainId.DomainId;
 import com.mt.common.domain.model.domain_event.DomainEvent;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,11 +10,14 @@ import lombok.Setter;
 public class SkuPatchedReplyEvent extends DomainEvent {
     private boolean emptyOpt;
     private long taskId;
+    public static final String name="SKU_PATCHED_REPLY_EVENT";
 
     public SkuPatchedReplyEvent(boolean emptyOpt, long taskId,String topic) {
         setEmptyOpt(emptyOpt);
         setTaskId(taskId);
         setInternal(false);
         setTopic(topic);
+        setDomainId(new DomainId(String.valueOf(taskId)));
+        setName(name);
     }
 }
